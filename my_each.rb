86283e79b340014a98f1c736aa40fe1ed7f60bf0
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-13 18:17:48 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,7 @@
-def my_each(words)
-  i = 0
-  while i < words.length
-    words.collect do |words|
-    return words [i]
-    i += i + 1
+
+
+def my_each(words)#
+  words.collect do |words|
+    return words
   end
-  words[0]
 end
